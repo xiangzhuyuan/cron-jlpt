c89ace4aa5a174f495acfa2c11b8cb0d29fb7cfc
xiang zhuyuan <xiangzhuyuan@gmail.com>
2015-12-23 14:35:39 +0900

    siji

---
+++ siji/sent.rb
@@ -0,0 +1,41 @@
+require 'twitter'
+
+
+BASE_HOME = __dir__
+
+def twi_conn
+  begin
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['CONSUMER_KEY']
+      config.consumer_secret     = ENV['CONSUMER_SECRET']
+      config.access_token        = ENV['ACCESS_TOKEN']
+      config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+    end
+  rescue => e
+    puts "Get twi conection failed with #{e.message}"
+  end
+end
+
+
+def sent_twi(twi)
+
+  begin
+    twi_conn.update(twi)
+    sleep(1)
+  rescue Twitter::Error::Forbidden => e
+    puts e.message
+  end
+end
+
+def sent_twi_with_image(content, image_path)
+  begin
+    puts "#{content}->#{image_path}"
+    twi_conn.update_with_media(content, File.new(image_path))
+  rescue Twitter::Error::Forbidden => e
+    puts e.message
+  end
+end
+
+File.foreach(File.join(__dir__, 'list.txt')).to_a.sample(5).each { |item|
+  sent_twi("#四字熟語 " + item)
+}

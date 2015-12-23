34e6daa82aeda2c8dac55811d04d2283aa88c06f
xiang zhuyuan <xiangzhuyuan@gmail.com>
2015-12-23 11:24:05 +0900

    add cron

---
+++ japanesetest4you/sent.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+export PATH=/home/matt/.rbenv/shims:$PATH
+
+
+export CONSUMER_KEY="A92HuuC2zgIBDbwWteMlQX3hK"
+export CONSUMER_SECRET="O6YLt1u3mIQOaBGw3LY91pQbpfdzpz5OdpnGyjzxkFhTmrIR26"
+export ACCESS_TOKEN="143062296-A9bdsRSqe9xb6iPcuTY6xZLFhTCfozQ5zIFc6rJZ"
+export ACCESS_TOKEN_SECRET="oFAJxfhXG73Bqi8icTa4glKkGd1SErLjcMgunNV0m7i4Y"
+
+
+DATE=`date +%Y-%m-%d`
+LOG_FILE="$(pwd)/log/${DATE}.log"
+echo $LOG_FILE
+
+#if [ ! -d /home/matt/cron-jlpt/log/ ]; then
+# mkdir -p /home/matt/cron-jlpt/log/
+# touch /home/matt/cron-jlpt/log/cron.log
+#fi
+#
+echo "start cron job at ${DATE}" >> $LOG_FILE
+ruby `pwd`/sent.rb >> $LOG_FILE
+echo "end cron job at ${DATE}" >> $LOG_FILE
